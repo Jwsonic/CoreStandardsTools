96519ea18a3583b0dfcf43029314174dd048f1bf
John Palgut <Jwsonic@gmail.com>
2014-08-14 17:02:14 -0500

    Used go-bindata to create binary blobs of the core standards data

--- main.go
+++ main.go
@@ -3,23 +3,22 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
+	"github.com/Jwsonic/CoreStandardsTools/bindata"
 	"github.com/Jwsonic/CoreStandardsTools/corestandards"
 	"os"
 )
 
 func main() {
+	data, err := bindata.Asset("data/math.xml")
 
-	f, err := os.Open("ela-literacy.xml")
-	if err != nil {
-		fmt.Println(err)
+	if len(data) == 0 || err != nil {
+		fmt.Println("Asset not found!")
 		os.Exit(1)
 	}
 
-	defer f.Close()
-
 	var standards corestandards.XMLLearningStandards
 
-	if err := xml.NewDecoder(f).Decode(&standards); err != nil {
+	if err := xml.Unmarshal(data, &standards); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
